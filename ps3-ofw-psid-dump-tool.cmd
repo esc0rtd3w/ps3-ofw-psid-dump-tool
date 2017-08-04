a08c08652b09fe91a5ecad52957d49ba01e3cb29
Jason Borowski <esc0rtd3w@gmail.com>
2017-08-03 20:30:35 -0400

    Fixed All Issues. Tested On OFW 4.81 PSID Retrieve

--- ps3-ofw-psid-dump-tool.cmd
+++ ps3-ofw-psid-dump-tool.cmd
@@ -18,12 +18,14 @@ set ip=0.0.0.0
 
 set ftpText="%temp%\ftpTextTemp.txt"
 
-set root=%~dp0   
+set root=%~dp0
 
 
 :start
 
 color 0e
+
+goto getPSID
 
 cls
 echo Make sure any previous PSID is removed before starting!
@@ -61,15 +63,15 @@ echo ps3>>%ftpText%
 echo bin>>%ftpText%
 echo cd /dev_hdd0/game/NPUP00030/USRDIR/APPDATA/>>%ftpText%
 echo mdelete PSID.dat>>%ftpText%
-echo quit>>%tempFile%
+echo quit>>%ftpText%
 
 ftp -n -s:%ftpText% %ip%
-del /f /q %tempFile%
+del /f /q %ftpText%
 
 
-:dtuC2O
+:dtuC2OS
 cls
-echo * DISABLE INTERNET CONNECTION ON RECIEVING CONSOLE BEFORE RUNNING DTU *
+echo * DISABLE INTERNET CONNECTION ON RECEIVING CONSOLE BEFORE RUNNING DTU *
 echo.
 echo.
 echo Now initiate a DTU from CFW to OFW
@@ -86,7 +88,7 @@ echo.
 echo *** If you are prompted about trophies, etc, select NO for each option ***
 echo.
 echo.
-echo Once the message "Waiting to transfer data" appears, switch over to the OFW console.
+echo Once "Waiting to transfer data" appears, switch over to the OFW console.
 echo.
 echo.
 echo.
@@ -96,28 +98,12 @@ echo.
 pause>nul
 
 
-:nfRun
+:dtuC2OR
 cls
-echo Now run the Netflix app and wait until it loads completely
-echo.
-echo *** It does not require internet or PSN access to create the needed file ***
+echo * DISABLE INTERNET CONNECTION ON RECEIVING CONSOLE BEFORE RUNNING DTU *
 echo.
 echo.
-echo Once Netflix has loaded, exit the app using the PS button
-echo.
-echo.
-echo When back at XMB, press ENTER for further instructions....
-echo.
-
-pause>nul
-
-
-:dtuC2O
-cls
-echo * DISABLE INTERNET CONNECTION ON RECIEVING CONSOLE BEFORE RUNNING DTU *
-echo.
-echo.
-echo Now initiate a DTU from OFW to CFW
+echo Now prepare to receive data from CFW console
 echo.
 echo.
 echo Located under Settings - System Settings - Data Transfer Utility
@@ -131,11 +117,78 @@ echo 3) Select YES to confirm the hard drive will be formatted
 echo.
 echo.
 echo.
-echo Once DTU is finished, press ENTER to continue....
+echo Once DTU is finished and you return to XMB, press ENTER to continue....
 echo.
 
 pause>nul
 
+
+:nfRun
+cls
+echo Now run the Netflix app on OFW console and wait until it loads completely
+echo.
+echo *** It does not require internet or PSN access to create the needed file ***
+echo.
+echo.
+echo.
+echo Once Netflix has loaded, exit the app using the PS button
+echo.
+echo.
+echo When back at XMB, press ENTER for further instructions....
+echo.
+
+pause>nul
+
+
+:dtuO2CS
+cls
+echo * DISABLE INTERNET CONNECTION ON RECEIVING CONSOLE BEFORE RUNNING DTU *
+echo.
+echo.
+echo Now initiate a DTU from OFW to CFW
+echo.
+echo.
+echo 1) Select OK and then Option 1 (Transfer data from this system....)
+echo.
+echo 2) Select OK again for connection confirm screen
+echo.
+echo.
+echo *** If you are prompted about trophies, etc, select NO for each option ***
+echo.
+echo.
+echo Once "Waiting to transfer data" appears, switch over to the CFW console.
+echo.
+echo.
+echo.
+echo To see CFW instructions, press ENTER to continue....
+echo.
+
+pause>nul
+
+
+:dtuO2CR
+cls
+echo * DISABLE INTERNET CONNECTION ON RECEIVING CONSOLE BEFORE RUNNING DTU *
+echo.
+echo.
+echo Now prepare to receive data from OFW console
+echo.
+echo.
+echo Located under Settings - System Settings - Data Transfer Utility
+echo.
+echo.
+echo 1) Select OK and then Option 2 (Transfer data from the other PS3....)
+echo.
+echo 2) Select YES to confirm all data will be erased
+echo.
+echo 3) Select YES to confirm the hard drive will be formatted
+echo.
+echo.
+echo.
+echo Once DTU is finished and you return to XMB, press ENTER to continue....
+echo.
+
+pause>nul
 
 
 
@@ -153,12 +206,12 @@ pause>nul
 echo user ps3>>%ftpText%
 echo ps3>>%ftpText%
 echo bin>>%ftpText%
-echo cd /dev_hdd0/game/NPUP00030/USRDIR/APPDATA/>>%ftpText%
-echo get PSID.dat %root%>>%ftpText%
-echo quit>>%tempFile%
+echo cd /dev_hdd0/game/NPUP00030/USRDIR/APPDATA>>%ftpText%
+echo get PSID.dat "%root%PSID.dat">>%ftpText%
+echo quit>>%ftpText%
 
 ftp -n -s:%ftpText% %ip%
-del /f /q %tempFile%
+del /f /q %ftpText%
 
 
 
